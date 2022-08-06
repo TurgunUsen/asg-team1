a07083330db54a20498246e163ffaa75aca49ac8
EC2 Default User <ec2-user@ip-172-31-44-24.eu-west-1.compute.internal>
2022-08-06 18:45:16 +0000

    Minor changes

--- asg.tf
+++ asg.tf
@@ -14,18 +14,23 @@ data "aws_ami" "packer" {
 }
 
 module "asg" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  name = "Team1-asg"
+  source = "terraform-aws-modules/autoscaling/aws"
+  name   = "Team1-asg"
 
   min_size                  = 1
   max_size                  = 99
   desired_capacity          = 1
   wait_for_capacity_timeout = 0
   health_check_type         = "EC2"
-  availability_zones	=  data.aws_availability_zones.all.names  # Use all AZs from this region
+  availability_zones        = data.aws_availability_zones.all.names # Use all AZs from this region
 
-  image_id          = data.aws_ami.packer.id  # This uses AMI found under your account
-  instance_type     = "t2.micro"
+  # Launch template
+  # launch_template_name        = "launch-template-team1"
+  # launch_template_description = "Launch template for ASG team1"
+  # update_default_version      = true
+
+  image_id          = data.aws_ami.packer.id # This uses AMI found under your account
+  instance_type     = "t3.micro"
   ebs_optimized     = true
   enable_monitoring = true
 
